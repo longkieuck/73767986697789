dfa2e1bf1f20974b3327a6d1ef986a5d170dfe21
KDLong <longkieuck@gmail.com>
2022-11-15 01:40:33 +0700

    final

--- js/myJs.js
+++ js/myJs.js
@@ -1,7 +1,7 @@
 const textConfig = {
   text1: "Hello NTMy1",
-  text2: "Lần đầu anh áp dụng cái này! Web làm nhanh nên hơi bất ổn thông kảm nha! Gét gô!",
-  text3: "Web page này là dành cho em. Người con gái Đúng's gu ._.",
+  text2: "Chuẩn bị đọc thơ này <3",
+  text3: "Hello Hello",
   text4: "Hi vọng mọi điều tốt đẹp và tuyệt vời nhất sẽ đến với em - My crush!",
   text5: "Mơ đi cưng!",
   text6: "Được nha!",
@@ -14,18 +14,25 @@ const textConfig = {
   text12: "Okii lunn <3",
 };
 const imageConfig = [
-  'https://scontent.fhan5-11.fna.fbcdn.net/v/t39.30808-6/309834556_3171169749879909_5566148389734582374_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=pXev21eu7eMAX_sOzx6&_nc_ht=scontent.fhan5-11.fna&oh=00_AfBqdfqwLX9zEUm7FgFXiUXs8EWWHvzxfN6b30O9_Qx_bQ&oe=636E10B5',
-  'https://scontent.fhan5-8.fna.fbcdn.net/v/t1.6435-9/121665566_2633071817023041_1259856502831211079_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=174925&_nc_ohc=igC9xJjxDocAX85PW04&_nc_ht=scontent.fhan5-8.fna&oh=00_AfDY-gZV0FHGz0ImA3qM0fImHS1XaI9-zuWVeKpGJnG8IA&oe=6390D412',
-  'https://scontent.fhan5-2.fna.fbcdn.net/v/t1.6435-9/122494005_2643549495975273_2110174463026514322_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=174925&_nc_ohc=UKgQlp4B0d8AX-LsVvz&_nc_ht=scontent.fhan5-2.fna&oh=00_AfCdvxkIxgD2_5vXMP0HFccB60xr-R_VrtIlZKYgf6b0mw&oe=6390BFED',
-  'https://scontent.fhan5-8.fna.fbcdn.net/v/t1.6435-9/169758736_2771382376525317_4051087190529829763_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=174925&_nc_ohc=CX2FBLH8IS0AX92Ze5y&_nc_ht=scontent.fhan5-8.fna&oh=00_AfB7rT-_HaXCDwmlV8Q5Z3rl92I6XU_rdwtNX1KpLKVqyg&oe=6390A344',
-  'https://scontent.fhan5-8.fna.fbcdn.net/v/t39.30808-6/271879313_2970830823247137_4225948491757987663_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=174925&_nc_ohc=B_y3wmb36I8AX97-QY_&_nc_ht=scontent.fhan5-8.fna&oh=00_AfADCNoyywJ_gakObv_SqkQsCZ1OB7tBdR6qhmQgP-vNwA&oe=636F219A',
-  'https://scontent.fhan5-8.fna.fbcdn.net/v/t39.30808-6/280483923_3056178228045729_8272129598389950866_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=174925&_nc_ohc=I2NiB3Qa4PYAX9UGQI9&_nc_oc=AQmnDr4p7i0LTZKY0G7vO6lWS1pYRQxRpQ2YHywQXFvR0BFaYuwpswmxsxMcHQZWfYs&_nc_ht=scontent.fhan5-8.fna&oh=00_AfCqQhFy_5PUpj3_aivRkjE1fW3e6po6jl3rpmxBsRKg0Q&oe=636E2325',
-  'https://scontent.fhan5-8.fna.fbcdn.net/v/t39.30808-6/283711482_3069161106747441_7879071787437631072_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=174925&_nc_ohc=KykNaGlQW6wAX87em2t&_nc_ht=scontent.fhan5-8.fna&oh=00_AfCyBmUkFtBbwwVYl3YOs4kka63yLdUKq6xrKvXq2APAFQ&oe=636E0172',
-  'https://scontent.fhan5-2.fna.fbcdn.net/v/t39.30808-6/287997410_3083425868654298_5455988160810743296_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=174925&_nc_ohc=blCcCLPGsBAAX9OmlFI&_nc_ht=scontent.fhan5-2.fna&oh=00_AfA52UUlyIBevJzFd6iiCuL8wt2Yg06FXhSw_w6ymFyqQw&oe=636F2EDD',
-  'https://scontent.fhan5-8.fna.fbcdn.net/v/t39.30808-6/302200911_3140880289575522_7397034184281682250_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=174925&_nc_ohc=8dhkd8P1mRYAX-OhjOD&tn=cg8HkFnB7FPP64vZ&_nc_ht=scontent.fhan5-8.fna&oh=00_AfBE053ZAb_BjYenn28t2P8soUFkk8LvjXGHN6GHZiHIcQ&oe=636DCA92',
-  'https://scontent.fhan5-8.fna.fbcdn.net/v/t39.30808-6/307327959_3147631135567104_4817136633695905969_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=174925&_nc_ohc=_wsOxQSm9C4AX99xrfU&_nc_ht=scontent.fhan5-8.fna&oh=00_AfCSG0nsIhDt-HJwJZvrunutkY9VmcrsDTC-P4BGi4jFOA&oe=636D5978',
+  'https://scontent.fhan5-11.fna.fbcdn.net/v/t39.30808-6/309834556_3171169749879909_5566148389734582374_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=pXev21eu7eMAX_sOzx6&_nc_ht=scontent.fhan5-11.fna&oh=00_AfBqdfqwLX9zEUm7FgFXiUXs8EWWHvzxfN6b30O9_Qx_bQ&oe=636E10B5'
   ]
-let index = 0, text = 'Vào chiều ngày 29/10/2022, chẳng hiểu đi lạc kiểu gì anh lại lạc ngay vào sân bóng. Chẳng hiểu có một cơn nắng nào đó lỡ va vào mắt làm anh say mất tiêu luôn. Xinh thực sự! Nhưng anh nghĩ là xinh như vậy kiểu gì cũng có người iêu ròi. Sau khi nhờ quân sư Thuỷ hỏi giúp thìiii! thực sự để mà nói là không biết đây có phải là bước đầu của sự tuyệt vời mà ô trời gửi tặng anh không!(Vui hết sức).';
+let index = 0, 
+text = `<span style="color:pink">Anh</span> muốn kể với em một câu chuyện
+<br><span style="color:pink">Yêu</span> một người rất khó? Phải không em
+<br><span style="color:pink">Em</span> là một bông hoa xinh đẹp nhất
+<br><span style="color:pink">Nhiều</span> mơ ước khát vọng tuổi đôi mươi
+<br>
+<br><span style="color:pink">Lắm</span> lúc anh ngồi một mình suy nghĩ
+<br><span style="color:pink">Trà</span> My liệu có thích mình không ha?
+<br><span style="color:pink">My</span> là một cái tên anh thương nhớ
+<br><span style="color:pink">À!</span> Không phải. Anh cất ở trong TYM`,
+text133 = `<br><span style="color:pink">Cho</span> anh được một lần hôn lên má
+<br><span style="color:pink">Anh</span> sẽ gửi lại cả cuộc đời anh
+<br><span style="color:pink">Pass</span> Facebook, insta và tiktok
+<br><span style="color:pink">Thử</span> một lần xao xuyến mãi không quên
+<br>
+<br><span style="color:pink">Việc</span> cuối cùng là hãy Say Đồng ý
+<br><span style="color:pink">Nha!</span> Nha em! Anh vẫn đợi em nè!`
 let iStatus= 0;
 $(document).ready(function () {
   // process bar
@@ -43,7 +50,7 @@ $(document).ready(function () {
   $("#yes").html(textConfig.text6);
   function firstQuestion() {
     $(".content").hide();
-    $(".cover-login").hide();
+    $("#status").hide()
     Swal.fire({
       title: textConfig.text1,
       text: textConfig.text2,
@@ -53,7 +60,8 @@ $(document).ready(function () {
       background: '#fff url("img/iput-bg.jpg")',
       imageAlt: "Custom image",
     }).then(function () {
-      $(".cover-login").show(200);
+      $(".content").show(1000);
+      $("#status").show(1000)
     });
   }
 
@@ -113,9 +121,7 @@ $(document).ready(function () {
     about1.style.color = 'pink';
     let about2 = document.getElementById("text4");
     about2.style.color = 'pink';
-    setInterval(() => {
-      loopImage()
-    }, 2000);
+    
   });
   // show popup
   $("#next").click(function () {
@@ -135,8 +141,9 @@ $(document).ready(function () {
   $("#status").click(function () {
    if(iStatus == 0){
     $("#text13").html(text);
+    $("#text133").html(text133);
     iStatus++;
-    $("#status").html('Tiếp này')
+    $("#status").html('')
     return
    }
    if(iStatus == 1){
@@ -163,7 +170,7 @@ $(document).ready(function () {
    }
 
    if(iStatus == 4){
-    text = text + ' <br>Hỏi cho vui vậy thôi chứ a tự công nhận là nhạt thật! Nhạt nhưng bù lại được cái yêu em thật lòng! Nhạt nhưng được cái muốn làm cho em vui cười mỗi khi nhắn tin, mỗi lúc bên anh nếu may mắn hơn thì có thể sẽ là mãi về sau này nữa! Sau thời gian tìm hiểu vừa xong ấy, anh đã chắc chắn được tình cảm của mình và đã đến lúc để thật nghiêm túc nói với em rùi(Trả lời bằng cách click vào button phía trên kia ha - <span style="color:pink">Em chỉ yêu anh - Còn cả thế giới cứ để anh lo - Làm người yêu anh nha?</span>)'
+    text = text + ' <br>Hỏi cho vui vậy thôi chứ a tự công nhận là nhạt thật! Nhạt nhưng bù lại được cái yêu em thật lòng! Nhạt nhưng được cái muốn làm cho em vui cười mỗi khi nhắn tin, mỗi lúc bên anh nếu may mắn hơn thì có thể sẽ là mãi về sau này nữa! Sau thời gian tìm hiểu vừa xong ấy, anh đã chắc chắn được tình cảm của mình và đã đến lúc để thật nghiêm túc nói với em rùi(Trả lời bằng cách click vào button phía trên kia ha - <span style="color:pink"></span>)'
     $("#text13").html(text);
     iStatus++;
     $("#status").html('')
@@ -179,38 +186,4 @@ $(document).ready(function () {
     // $("#next").attr('src','https://scontent.fhan5-8.fna.fbcdn.net/v/t39.30808-6/306834128_201831088932568_29213235053674702_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=174925&_nc_ohc=IInDuFVUkTsAX8PsmJE&_nc_ht=scontent.fhan5-8.fna&oh=00_AT_p-vPTpoz5wRBgHZXuv2RSqS7PHTJ6QvK9_ITC31otag&oe=635047B6');
   });
 
-  $(document).on('input', '.username', function(e){
-    
-    if(e.target.value.toLowerCase() == 'my crush'){
-      $("#title-login").html('Hello ' + e.target.value+'<br>You are so Beautiful <3');
-      let about9 = document.getElementById("title-login");
-      about9.style.color = 'pink';
-      about9.style.fontSize = '35px'
-
-      let lbuser = document.getElementById("lbuser");
-      let lbpass = document.getElementById("lbpass");
-      lbuser.style.color = 'pink';
-      lbpass.style.color = 'pink';
-    }else{
-      let about9 = document.getElementById("title-login");
-      about9.style.color = 'black';
-      about9.style.fontSize = '25px'
-      $("#title-login").html('Hello ' + e.target.value);
-
-      let lbuser = document.getElementById("lbuser");
-      let lbpass = document.getElementById("lbpass");
-      lbuser.style.color = 'black';
-      lbpass.style.color = 'black';
-    }
-  });
-  $("#submit").click(function () {
-   let username = document.getElementById('username').value;
-   let password = document.getElementById('password').value; 
-   if(username.toLowerCase() == 'my crush' && password.toLowerCase() == 'ntmy1'){
-    $(".content").show();
-    $(".cover-login").hide();
-   }else{
-    alert('Điền đúng mới vào được bạn ưiiii :v')
-   }
-  });
 });
